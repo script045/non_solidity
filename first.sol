721d04f594e61b88b964714849ab61bf223aaf9b
script045 <devscript045@gmail.com>
2025-03-07 21:56:35 +0600

    init

--- first.sol
+++ first.sol
@@ -2,5 +2,13 @@
 pragma solidity ^0.8.8;
 
 contract First{
-    string public welcomeMessage = "Hello Web3";
+
+    int public num = 10;
+    string public greeting = "Welcome";
+    bool public isActive = false;
+    address public myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    function getNumber() public view returns (int){
+        return num;
+    }
+ 
 }

1dbf1aa8746921061254ecb2d8bc95db6c2f4211
script045 <devscript045@gmail.com>
2025-03-07 22:04:19 +0600

    init

--- first.sol
+++ first.sol
@@ -3,12 +3,8 @@ pragma solidity ^0.8.8;
 
 contract First{
 
-    int public num = 10;
-    string public greeting = "Welcome";
-    bool public isActive = false;
-    address public myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
-    function getNumber() public view returns (int){
-        return num;
-    }
+    string public studentName;
+    int public studentAge;
+    string public courseName;
  
 }

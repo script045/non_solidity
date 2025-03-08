3f2c638ac3700b3356e8388e5f5dfc09fef279eb
script045 <devscript045@gmail.com>
2025-03-08 21:53:25 +0600

    init

--- first.sol
+++ first.sol
@@ -1,26 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-contract Array{
-    //fixed size array
-    uint[3] public scores = [85, 80, 90];
-
-    // dynamic size arrays
-    uint[] public dynamicScores;
-
-    function addScore(uint score) public {
-        dynamicScores.push(score); // add a new number to the end
+contract TodoList{
+    struct Task {
+        string content;
+        bool completed;
     }
 
-    function getScore(uint index) public view returns (uint) {
-        return dynamicScores[index];
-    }
- 
-    function getNumberOfScores() public view returns (uint) {
-        return dynamicScores.length;
+    Task[] public tasks;
+
+    // add a new task
+    function addTask(string memory _content) public {
+        tasks.push(Task(_content, false));
     }
 
-    function removeLastScode() public {
-        dynamicScores.pop();
+    //mark task as completed
+    function completeTask(uint taskIndex) public {
+        require(taskIndex < tasks.length, "The task does not exist");
+        tasks[taskIndex].completed = true;
     }
+
+    //get the task
+    function getTask(uint taskIndex) public view returns (string memory content, bool completed) {
+        require(taskIndex < tasks.length, "The task does not exist");
+        Task storage task = tasks[taskIndex];
+        return (task.content, task.completed);
+    }
+
+    // memory is temporay memory
+    // storage is permanent memory
 }

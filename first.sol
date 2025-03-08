4dbc88a752603fe409ecd0cc42d398381e2f8d45
script045 <devscript045@gmail.com>
2025-03-08 18:03:27 +0600

    init

--- first.sol
+++ first.sol
@@ -1,30 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-contract MathematicalOperation {
-    uint public score = 0;
-
-    //Addition
-    function addToScore(uint points) public {
-        score = score+points; // DRY _ Dont Repeat Yourself
-        // score += points;
-    } 
-
-    //Multiplication
-    function multiplyScore(uint number) public {
-        score = score*number;
-        // score *= number;
+contract InvestmentCalculator {
+    function calculatorROI (uint investment, uint returnAmount) public pure returns(uint) {
+        return (returnAmount *100) /investment;
     }
 
-    //Substraction
-    function subtractFunction(uint points) public {
-        require(score >= points, "score cannot be negative");
-        score = score - points;
-    }
-
-    //Division
-    function dividedScore(uint number) public {
-        require(number > 0, "Cannot divide by zero");
-        score = score/number;
+    function calculateInterest(uint principal, uint rate) public pure returns (uint){
+        return (principal * rate)/100;
     }
 }

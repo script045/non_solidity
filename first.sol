2819e41985dc429de2a24fa799f061c5a517cf61
script045 <devscript045@gmail.com>
2025-03-08 12:37:56 +0600

    "init"

--- first.sol
+++ first.sol
@@ -3,19 +3,27 @@ pragma solidity ^0.8.8;
 
 contract First{
 
-    // how to data store in solidity
+    //varialble & data types
+    // numbers
+    int public num1 = -10;
+    uint public num2 = 20;
 
-    string public studentName;
-    int public studentAge;
-    string public courseName;
-    bool public isEnrolled;
+    uint8 public smallNumber = 255;
+    uint16 public mediumNumber = 50000;
+    uint256 public largeNumber = 5000000;
 
-    function setProfile(string memory name, int age, string memory course ) public {
-        studentName = name ;
-        studentAge=age;
-        courseName=  course ;
-        isEnrolled =  true;
-    }
+   //text
 
- 
+   string public message = "hello NonAcademian";
+   string public muName = "Ai Arif";
+
+   //boolean
+   bool public isActive = true;
+   bool public isComplete = false;
+
+   //address 
+   address public myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+
+
 }
---
+++ readme.md
@@ -0,0 +1,36 @@
+# M32.2 Variables & Data types
+
+
+    ### numbers
+
+    
+    ```
+    int public num1 = -10;
+    uint public num2 = 20;
+
+
+    uint8 public smallNumber = 255;
+    uint16 public mediumNumber = 50000;
+    uint256 public largeNumber = 5000000;
+    
+    ```
+
+    ###  text
+    ```
+    string public message = "hello NonAcademian";
+    string public muName = "Ai Arif";
+    ```
+
+
+    ### boolean
+    ```
+    bool public isActive = true;
+    bool public isComplete = false;
+    ```
+
+
+   ### address 
+    ```
+    address public myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    ```
+   

17c156653b06e8d20d90b66b1cbf59795be229eb
script045 <devscript045@gmail.com>
2025-03-10 06:08:22 +0600

    init

--- first.sol
+++ first.sol
@@ -1,32 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-contract TodoList{
-    struct Task {
-        string content;
-        bool completed;
+contract Mapping {
+    // key-value store
+    // key:value
+
+    //// declearing mapping
+    // mapping (keyType=>valueType) public mappingName;
+
+    mapping(address => uint) public balances;
+
+    //nested mapping
+    mapping(address => mapping(address => uint)) public allowances;
+
+    function updateBalance(uint newBalance) public {
+        balances[msg.sender] = newBalance;
     }
 
-    Task[] public tasks;
-
-    // add a new task
-    function addTask(string memory _content) public {
-        tasks.push(Task(_content, false));
+    function getBalance(address user) public view returns (uint){
+        return balances[user];
     }
-
-    //mark task as completed
-    function completeTask(uint taskIndex) public {
-        require(taskIndex < tasks.length, "The task does not exist");
-        tasks[taskIndex].completed = true;
-    }
-
-    //get the task
-    function getTask(uint taskIndex) public view returns (string memory content, bool completed) {
-        require(taskIndex < tasks.length, "The task does not exist");
-        Task storage task = tasks[taskIndex];
-        return (task.content, task.completed);
-    }
-
-    // memory is temporay memory
-    // storage is permanent memory
 }

d63429726b59a60931eaf2f480597c5c88a4a589
script045 <devscript045@gmail.com>
2025-03-10 07:41:41 +0600

    init

--- first.sol
+++ first.sol
@@ -1,21 +1,52 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-contract StudentRegistry {
-    //Mapping from student id to their score
-    mapping (uint => uint) public studentScore;
+contract SchoolSystem{
 
-    //Store a  student's score
-    function setScore(uint studentId, uint score) public {
-        studentScore[studentId] = score;
+    struct Student {
+        string name;
+        uint grade;
+        bool active;
     }
 
-    function getScore(uint studentId) public view returns (uint){
-        return studentScore[studentId];
+    // array to keep track of all students ids
+    uint[] public studentIds;
+     mapping (uint => Student) public students;
+
+    // mappping from student id to array of test scores
+    mapping(uint => uint[]) public studentScores;
+
+    // add a new student
+    function addStudent(uint id, string memory name) public {
+        students[id] = Student(name, 0, true);
+        studentIds.push(id);
     }
 
-    //check if student passed (score >= 60)
-    function hasPassed (uint studentId) public view returns (bool) {
-        return studentScore[studentId]>= 60;
+    // add test score for a student
+    function addScore(uint studentId, uint score) public {
+        require(students[studentId].active, "Student not found");
+
+        studentScores[studentId].push(score);
+
+        //update grade based on average
+        uint total = 0;
+        uint[] memory scores = studentScores[studentId];
+        for(uint i=0; i<scores.length; i++){
+            total += scores[i];    
+        }
+         students[studentId].grade = total/scores.length;
+        
     }
+
+    //get all score for a student
+    function getScores(uint studentId) public view returns (uint[] memory){
+        return studentScores[studentId];
+    }
+
+
+    //get total number of students
+    function getTotalStudents() public view returns(uint){
+        return studentIds.length;
+    }
+
 }
--- readme.md
+++ readme.md
@@ -1,26 +1,9 @@
 # M40: Part 6 Solidity
-## M40.1 Student Resistry - Mapping Project
+## M40.2 Array & Mappig Project
 
 
 
 
 ```
-contract StudentRegistry {
-    //Mapping from student id to their score
-    mapping (uint => uint) public studentScore;
 
-    //Store a  student's score
-    function setScore(uint studentId, uint score) public {
-        studentScore[studentId] = score;
-    }
-
-    function getScore(uint studentId) public view returns (uint){
-        return studentScore[studentId];
-    }
-
-    //check if student passed (score >= 60)
-    function hasPassed (uint studentId) public view returns (bool) {
-        return studentScore[studentId]>= 60;
-    }
-}
 ```
